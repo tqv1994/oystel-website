36810b718a2c0d2e115cd900741ca4d0d965499b
Vuong Tran <vuongtq@absoft.com.vn>
2021-09-06 23:04:31 +0700

    run on vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
-// import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
