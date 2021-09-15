ae71491592fcaf230e8dd3c7c3f9fa195dccac14
Vuong Tran <vuongtq@absoft.com.vn>
2021-09-15 20:22:03 +0700

    Build vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -7,13 +7,13 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-      adapter: adapter({
-          // default options are shown
-          pages: 'build',
-          assets: 'build',
-          fallback: null
-      }),
-      // adapter: adapter(),
+      // adapter: adapter({
+      //     // default options are shown
+      //     pages: 'build',
+      //     assets: 'build',
+      //     fallback: null
+      // }),
+      adapter: adapter(),
     target: '#svelte',
 
     vite: {
