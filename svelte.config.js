e0019cc712e4411aa82dc047ba33588216e33d2b
VuongTQ <vuongtq@absoft.com.vn>
2021-09-16 10:56:56 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       //     pages: 'build',
       //     assets: 'build',
       //     fallback: null
-      // }),
+      // })1,
       adapter: adapter(),
     target: '#svelte',
 
