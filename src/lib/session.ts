81f2cf197d27d02963400c7232afa6b854bb49f3
VuongTQ <vuongtq@absoft.com.vn>
2021-10-06 15:40:28 +0700

    Update session.ts

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,26 +1,22 @@
+import { Request } from '@sveltejs/kit';
 import * as cookie from 'cookie';
-import type { Request, Response } from '@sveltejs/kit';
 
-export function sessionCookieFromRequest(req: Request): string {
-  // console.log(request.headers.cookie)
+export function getSessionCookieFromRequest(req: Request): string {
   const cookies = cookie.parse(req.headers.cookie || '');
-  // console.log(cookies)
   const { session, 'session.sig': sig } = cookies;
-  // console.log('session', session, sig)
   if (session && sig) {
-    // console.log('we have session cookie...');
     return `session=${session};session.sig=${sig}`;
   }
   return '';
 }
 
-export function sessionCookieFromResponse(
-  res: Response,
+export function filterResponseHeaders(
+  responseHeaders: Headers,
 ): Record<string, string> {
-  const setCookieHeader = (res.headers as any).raw()['set-cookie'];
+  const setCookie = (responseHeaders as any).raw()['set-cookie'];
   const headers: Record<string, string> = {};
-  if (setCookieHeader) {
-    headers['set-cookie'] = setCookieHeader;
+  if (setCookie) {
+    headers['set-cookie'] = setCookie;
   }
   return headers;
 }
